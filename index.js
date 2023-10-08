a06a9154362f4e18d11514392308e6c01c0ae5d0
jordanswp <61299892+jordanswp@users.noreply.github.com>
2023-10-08 20:40:34 +0800

    expanded on logic

--- index.js
+++ index.js
@@ -6,5 +6,15 @@ submitButton.addEventListener("click", function () {
   uncleanContent = inputField.value;
 
   // clean content
+  splitContent = uncleanContent.split(/\r?\n/);
+
+  const headers = ["H1", "H2", "H3", "H3"];
+
+  splitContent.forEach((line) => {
+    headers.forEach;
+  });
+  console.log(splitContent);
+
+  // output cleaned content
   outputField.value = uncleanContent;
 });
