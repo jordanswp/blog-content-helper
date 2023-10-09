29b665e90e4717d31d7406ea0cf6cb260612d13c
Firspage-Jordan <jordan@firstpagedigital.sg>
2023-10-09 15:33:14 +0800

    added JS logic to replace p tags to header tags and remove header texts

--- index.js
+++ index.js
@@ -3,18 +3,60 @@ const inputField = document.querySelector(".input--text-area");
 const outputField = document.querySelector(".output--text-area");
 
 submitButton.addEventListener("click", function () {
-  uncleanContent = inputField.value;
-
+  let uncleanContent = inputField.value;
   // clean content
   splitContent = uncleanContent.split(/\r?\n/);
-
-  const headers = ["H1", "H2", "H3", "H3"];
-
-  splitContent.forEach((line) => {
-    headers.forEach;
-  });
   console.log(splitContent);
 
+  const headers = ["H1", "H2", "H3", "H4"];
+
+  let cleanedContent = splitContent.map((line) => {
+    headers.some((header) => {
+      if (line.includes(header)) {
+        // console.log(`${line} contains ${header}`);
+        line = line
+          .replace(/<strong>/g, "")
+          .replace(/<\/strong>/g, "")
+          .replace(`${header} `, "")
+          .replace("<p>", `<${header.toLowerCase()}><strong>`)
+          .replace("</p>", `</strong></${header.toLowerCase()}>`);
+
+        // console.log(line);
+      }
+    });
+    return line;
+  });
+
+  // console.log(cleanedContent);
+
+  cleanContentJoined = cleanedContent.join("\r\n");
+
   // output cleaned content
-  outputField.value = uncleanContent;
+  outputField.value = cleanContentJoined;
+
+  // console.log(splitContent);
+
+  // splitContent.forEach((line) => {
+  //   headers.some((header) => {
+  //     // check if line contains H1, H2, H3 or H4
+  //     if (line.includes(header)) {
+  //       console.log(line);
+  //       // replace p tag with opening header tag and remove the header text
+  //       line = line
+  //         .replace(/<strong>/g, "")
+  //         .replace(/<\/strong>/g, "")
+  //         .replace("<p>", `<${header.toLowerCase()}><strong>`)
+  //         .replace("</p>", `</strong></${header.toLowerCase()}>`);
+  //     }
+  //     return line;
+  //   });
+  //   return line;
+  // });
+
+  // console.log(cleanedContent);
+
+  // cleanContentJoined = cleanedContent.join("\r\n");
+
+  // output cleaned content
+  // outputField.value = cleanContentJoined;
 });
